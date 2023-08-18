5162763acc38f653261a4f9bf32aceb2d9d6686e
Mohameedibrahim <110037333+Mohameedibrahim@users.noreply.github.com>
2023-08-18 12:07:31 +0200

    updates dropDowns more dynamic and make two dropdowns worked

--- js/main.js
+++ js/main.js
@@ -1,31 +1,58 @@
-  // Get all the toggle buttons and dropdown contents
-  const toggleButtons = document.querySelectorAll('.dropdown-toggles');
-  const dropdownContents = document.querySelectorAll('.dropdown-content');
 
-  // Close all dropdowns except for the specified index
-  function closeOtherDropdowns(exceptIndex) {
-    dropdownContents.forEach((content, index) => {
-      if (index !== exceptIndex) {
-        content.classList.remove('active');
+document.addEventListener("DOMContentLoaded", function() {
+  const containers = document.querySelectorAll(".dropdown-container");
+
+  containers.forEach(container => {
+    const button = container.querySelector(".dropdown-button");
+    const buttonDots = container.querySelector(".dropdown-button-dots");
+
+    const dropdownContent = container.querySelector(".dropdown-content");
+    const dropdownContent2 = container.querySelector(".dropdown-content2");
+
+
+    button.addEventListener("click", function(event) {
+      event.stopPropagation();
+      closeOtherDropdowns(container);
+      dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+      dropdownContent2.style.display =  dropdownContent.style.display === "block" ? "none" : "";
+
+    });
+
+    buttonDots.addEventListener("click", function(event) {
+      event.stopPropagation();
+      closeOtherDropdowns(container);
+      dropdownContent2.style.display = dropdownContent2.style.display === "block" ? "none" : "block";
+      dropdownContent.style.display =  dropdownContent2.style.display === "block" ? "none" : "";
+
+    });
+  });
+
+  document.addEventListener("click", function(event) {
+    closeAllDropdowns();
+  });
+
+  function closeOtherDropdowns(currentContainer) {
+    containers.forEach(container => {
+      if (container !== currentContainer) {
+        const dropdownContent = container.querySelector(".dropdown-content");
+        const dropdownContent2 = container.querySelector(".dropdown-content2");
+
+        dropdownContent.style.display = "none";
+        dropdownContent2.style.display = "none";
+
       }
     });
   }
 
-  // Add click event listeners to each toggle button
-  toggleButtons.forEach((toggleButton, index) => {
-    toggleButton.addEventListener('click', () => {
-      // Close other open dropdowns   
-        closeOtherDropdowns(index);
-        // Toggle the 'active' class on the clicked dropdown content
-        dropdownContents[index].classList.toggle('active');
-      
+  function closeAllDropdowns() {
+    const allDropdowns = document.querySelectorAll(".dropdown-content");
+    const allDropdowns2 = document.querySelectorAll(".dropdown-content2");
 
+    allDropdowns.forEach(dropdown => {
+      dropdown.style.display = "none";
     });
-  });
-
-  // Close all dropdowns when clicking outside of them
-  document.addEventListener('click', (event) => {
-    if (!event.target.matches('.dropdown-toggles') && !event.target.closest('.dropdown-content')) {
-      closeOtherDropdowns(-1);
-    }
-  });
+    allDropdowns2.forEach(dropdown => {
+      dropdown.style.display = "none";
+    });
+  }
+});
